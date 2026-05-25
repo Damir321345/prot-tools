70cebf1521a088e29d4893a42a8130cba7dbed92
Damir321345 <damir.abdildanov@bk.ru>
2026-05-25 22:42:10 +0500

    init: рабочий прототип Prot Tools v1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Damir321345/prot-tools
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Prot Tools v1")
+	fmt.Println("Рандомайзер токов запущен")
+}
